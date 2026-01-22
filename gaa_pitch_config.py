d73f36fbe783b18a5b0d218880c4f606e054af42
ciarangray0 <ciarangray0@gmail.com>
2026-01-22 14:44:54 +0000

    scipt to identify keypoints in a frame added and pitch config updated

--- gaa_pitch_config.py
+++ gaa_pitch_config.py
@@ -1,14 +1,14 @@
 # gaa_pitch_config.py
 
-GAA_PITCH_LENGTH = 145.0  # meters  (y)
+GAA_PITCH_LENGTH = 140.0  # meters  (y)
 GAA_PITCH_WIDTH  = 85.0   # meters (x)
 
 GAA_PITCH_VERTICES = {
     # Corners
     "corner_tl": (0.0, 0.0),
     "corner_tr": (85.0, 0.0),
-    "corner_bl": (0.0, 145.0),
-    "corner_br": (85.0, 145.0),
+    "corner_bl": (0.0, 140.0),
+    "corner_br": (85.0, 140.0),
 
     # Goal centers
     "top_goal_lp": (39.25, 0.0),
@@ -27,12 +27,18 @@ GAA_PITCH_VERTICES = {
     "left_13m_box_top": (33.0, 13.0),
     "right_13m_box_bottom": (52.0, GAA_PITCH_LENGTH - 13.0),
     "right_13m_box_top": (52.0, 13.0),
+    "left_endline_13m_box_bottom": (33.0, GAA_PITCH_LENGTH),
+    "left_endline_13m_box_top": (33.0, 0.0),
+    "right_endline_13m_box_bottom": (52.0, GAA_PITCH_LENGTH),
+    "right_endline_13m_box_top": (52.0, 0.0),
 
     # small arc
-    "left_small_arc_bottom": (33.0, GAA_PITCH_LENGTH - 20.0),
-    "left_small_arc_top": (33.0, 20.0),
-    "right_small_arc_bottom": (52.0, GAA_PITCH_LENGTH - 20.0),
-    "right_small_arc_top": (52.0, 20.0),
+    "left_small_arc_bottom": (29.5, GAA_PITCH_LENGTH - 20.0),
+    "left_small_arc_top": (29.5, 20.0),
+    "right_small_arc_bottom": (55.5, GAA_PITCH_LENGTH - 20.0),
+    "right_small_arc_top": (55.5, 20.0),
+    "small_arc_top_top": (42.5, 33.),
+    "small_arc_top_bottom": (42.5, GAA_PITCH_LENGTH - 33.0),
 
     # 13 m line
     "left_13m_line_bottom": (0.0, GAA_PITCH_LENGTH - 13.0),
